895df6a2e0bc4e0973ee1720c73dc38e339221c1
julia <jerlo180908@gmail.com>
2026-03-02 11:38:04 -0300

    Exercício 9 - Valor total carrinho de compras

---
+++ exercicio09.js
@@ -0,0 +1,12 @@
+//Valor total carrinho de compras
+
+const carrinho = [
+  { produto: "Notebook", preco: 4500.0, quantidade: 1 },
+  { produto: "Mouse", preco: 150.0, quantidade: 2 },
+  { produto: "Teclado", preco: 300.0, quantidade: 1 }
+];
+
+const totalCarrinho = carrinho.reduce((acumulador, item) => {
+    return acumulador + item.preco * item.quantidade;
+}, 0);
+console.log(totalCarrinho);
