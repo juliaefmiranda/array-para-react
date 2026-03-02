b5a34be57b1810cea0debd3add4bc3b7bc70c956
julia <jerlo180908@gmail.com>
2026-03-02 11:31:26 -0300

    Exercício 8 - Emails ativos

---
+++ exercicio08.js
@@ -0,0 +1,10 @@
+//Emails ativos
+
+const clientes = [
+  { nome: "Lucas", email: "lucas@test.com", ativo: true },
+  { nome: "Julia", email: "julia@test.com", ativo: false },
+  { nome: "Marcos", email: "marcos@test.com", ativo: true }
+];
+
+const emailsAtivos = clientes.filter((cliente) => cliente.ativo === true).map((cliente) => cliente.email);
+console.log(emailsAtivos);
