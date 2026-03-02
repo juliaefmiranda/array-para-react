30e0f5cd40206f43c4168c0dfc7448257b269f20
julia <jerlo180908@gmail.com>
2026-03-02 11:07:21 -0300

    Exercício 5 - Tarefas pendentes

---
+++ exercicio05.js
@@ -0,0 +1,10 @@
+//Tarefas pendentes
+
+const tarefas = [
+  { id: 1, descricao: "Estudar React", concluida: true },
+  { id: 2, descricao: "Configurar Webpack", concluida: false },
+  { id: 3, descricao: "Entender Reduce", concluida: false }
+];
+
+const tarefasPendentes = tarefas.filter((tarefa) => tarefa.concluida === false);
+console.log(tarefasPendentes);
